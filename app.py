024c651cc5734ffdd1891ae24f4d5c6f6822a973
Diogo Nunes <diogovaznunes@gmail.com>
2018-09-09 21:27:42 +0100

    update db business logic

--- app.py
+++ app.py
@@ -1,22 +1,13 @@
 from flask import Flask
 from flask import jsonify
 from bson.objectid import ObjectId
-from bson import json_util
 import datetime
 import json
+from scraper import get_top_5
 from flask_pymongo import PyMongo
 
 
-app = Flask(__name__)
-#app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
-#app.config["MONGO_URI"] = "mongodb+srv://switch_admin:switch_admin2@flask-switch-larnu.mongodb.net/test?retryWrites=true"
-app.config["MONGO_URI"] = "mongodb+srv://switch_admin:switch_admin2@flask-switch-larnu.mongodb.net/dbswitch"
-mongo = PyMongo(app)
-
-
 class JSONEncoder(json.JSONEncoder):
-    ''' extend json-encoder class'''
-
     def default(self, o):
         if isinstance(o, ObjectId):
             return str(o)
@@ -24,28 +15,40 @@ class JSONEncoder(json.JSONEncoder):
             return str(o)
         return json.JSONEncoder.default(self, o)
 
+
+def update_database(websites):
+    names = []
+    for website in websites:
+        mongo.db.websites.replace_one({'name': website['name']}, website, upsert=True)
+        names.append(website['name'])
+    mongo.db.websites.delete_many({"name": {"$nin": names}})
+
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://switch:switch1@flask-switch-larnu.mongodb.net/dbswitch"
+mongo = PyMongo(app)
 app.json_encoder = JSONEncoder
+update_database(get_top_5())
+
 
 @app.route("/")
 def hello():
-    mongo.db.users.insert_one({'name': 'david'})
-    david = mongo.db.users.find({'name': 'david'})
-    print(david)
-    json_res = []
-    for d in david:
-        print(d)
-        json_res.append(d)
-    return jsonify(json_res)
+    return "Hello"
+
+
+@app.route("/up")
+def hello():
+    return "Hello"
 
 
 @app.route("/top5")
-def asd():
-    return mongo.db.websites.find()
+def get_websites():
+    websites = list(mongo.db.websites.find())
+    json_res = []
+    for website in websites:
+        json_res.append(website)
+    return jsonify(json_res)
 
 
 if __name__ == "__main__":
-    print("asd")
     app.run()
-
-    print(mongo.db.users.insert_one({'name': 'david'}).inserted_id)
-    print("asd2")
